16ee7ade2f87ef09ab249ae15e3f1d62d20de8d9
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2024-11-09 18:09:49 +0530

    Rat chases its cheese Path

--- Recurssion/Subsequence.java
+++ Recurssion/Subsequence.java
@@ -3,37 +3,37 @@ import java.util.*;
 public class Subsequence{
     public static void main(String[] args){
         String s = "cbbd";
-        List<String> l = new ArrayList<>();
-        System.out.println(subSequence(s,"", l));
+        // List<String> l = new ArrayList<>();
+        System.out.println(subSequence(s,""));
 
-        String x = l.get(0);
-        int len = x.length();
-        int p = 0;
-        for(int i=0;i<l.size();i++){
-            String y = l.get(i);
-            int lent = y.length();
-            if(lent > len){
-                len = lent;
-                p =i;
-            }
-        }
-        System.out.println(l.get(p));
+        // String x = l.get(0);
+        // int len = x.length();
+        // int p = 0;
+        // for(int i=0;i<l.size();i++){
+        //     String y = l.get(i);
+        //     int lent = y.length();
+        //     if(lent > len){
+        //         len = lent;
+        //         p =i;
+        //     }
+        // }
+        // System.out.println(l.get(p));
 
     }
     //static int count =0;
-    public static int subSequence(String s,String ans, List<String> l){
+    public static int subSequence(String s,String ans){
 
         if(s.length() ==0){
-            if(isPalindrome(ans)== true){
-                l.add(ans);
-            }
+            // if(isPalindrome(ans)== true){
+            //     l.add(ans);
+            // }
             System.out.print(ans+" ");
             //count++;
             return 1;
         }
         char ch = s.charAt(0);
-        int a1 = subSequence(s.substring(1),ans, l);
-        int b1 = subSequence(s.substring(1),ans+ch, l);
+        int a1 = subSequence(s.substring(1),ans);
+        int b1 = subSequence(s.substring(1),ans+ch);
         return a1+b1;
     }
     public static boolean isPalindrome(String s){
